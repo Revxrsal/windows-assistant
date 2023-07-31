d9ff2374578baf663a1a9e39950ab042696962bf
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-07-31 22:32:36 +0300

    add content to the initial screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
     return (
-        <main>
-            <span>Hello, world!</span>
+        <main class="flex h-full flex-col">
+            <p class="mx-12 my-7 text font-bold text-xl">New shortcut</p>
+            <div class={"justify-center flex flex-col title m-12 mt-0"}>
+                <input type="text" placeholder={"Shortcut name..."}
+                       class="
+                       border-none outline-none
+                       text
+                       bg-gray-100 dark:bg-stone-800"/>
+            </div>
         </main>
     )
 }
