5f5ac76741f98f0141e59b3e72c892f57d155e8e
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-07-31 22:38:10 +0300

    add colors if the path is currently active

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,20 +3,36 @@ import {BsDatabaseAdd, BsFolder, BsGear, BsPinMapFill} from "solid-icons/bs";
 import SidebarTitle from "~/components/sidebar/SidebarTitle";
 import HorizontalDivider from "~/components/HorizontalDivider";
 import SidebarSubtitle from "~/components/sidebar/SidebarSubtitle";
+import {useLocation} from "@solidjs/router";
 
 export default function Sidebar() {
+    const location = useLocation();
+    const isPathOpen = (path: string) => location.pathname.startsWith(path)
     return (
         <>
             <div class="sidebar">
                 <SidebarTitle title={"Windows Assistant"}/>
                 <HorizontalDivider/>
-                <SidebarItem label={"New"} icon={<BsDatabaseAdd size={24}/>} />
-                <SidebarItem label={"My Shortcuts"} icon={<BsPinMapFill size={24}/>}/>
-                <SidebarItem label={"Quick Actions"} icon={<BsGear size={24}/>}/>
-                <SidebarSubtitle title={"Folders"}/>
-                <SidebarItem label={"Travel"} icon={<BsFolder size={24}/>}/>
-                <SidebarItem label={"Photos"} icon={<BsFolder size={24}/>}/>
-                <SidebarItem label={"Fitness"} icon={<BsFolder size={24}/>}/>
+                <SidebarItem
+                    label={"New"}
+                    icon={<BsDatabaseAdd size={24}/>}
+                    isActive={isPathOpen("/")}
+                />
+                <SidebarItem
+                    label={"My Shortcuts"}
+                    icon={<BsPinMapFill size={24}/>}
+                    isActive={isPathOpen("/my-shortcuts")}
+                />
+                <SidebarItem
+                    label={"Quick Actions"}
+                    icon={<BsGear size={24}/>}
+                    isActive={isPathOpen("/my-actions")}
+                />
+
+                {/*<SidebarSubtitle title={"Folders"}/>*/}
+                {/*<SidebarItem label={"Travel"} icon={<BsFolder size={24}/>}/>*/}
+                {/*<SidebarItem label={"Photos"} icon={<BsFolder size={24}/>}/>*/}
+                {/*<SidebarItem label={"Fitness"} icon={<BsFolder size={24}/>}/>*/}
             </div>
         </>
     )
--- src/components/sidebar/SidebarItem.tsx
+++ src/components/sidebar/SidebarItem.tsx
@@ -3,12 +3,14 @@ import {ComponentProps, JSX, splitProps} from "solid-js";
 export interface SidebarItemProps extends ComponentProps<"div"> {
     label?: JSX.Element
     icon?: JSX.Element
+    isActive?: boolean
 }
 
 export default function SidebarItem(props: SidebarItemProps) {
     const [local, ...[divProps]] = splitProps(props, ["label", "icon"])
+    const activeClass = () => props.isActive ? "bg-blue-600" : ""
     return (
-        <div class={"sidebar-item"} {...divProps}>
+        <div class={`sidebar-item ${activeClass()}`} {...divProps}>
             {/* Some icons are colored using 'text', others are with 'fill' */}
             <div class={"text-gray-100 fill-gray-100"}>
                 {local.icon}
