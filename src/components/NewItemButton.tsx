ac6439698f63279675fd2adf899125464d936c25
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-01 13:40:15 +0300

    allow button props on NewItemButton

--- src/components/NewItemButton.tsx
+++ src/components/NewItemButton.tsx
@@ -1,12 +1,14 @@
 import BlockIcon from "~/components/blocks/BlockIcon";
 import {BsPlusLg} from "solid-icons/bs";
+import {ComponentProps, splitProps} from "solid-js";
 
-export interface NewItemButtonProps {
+export interface NewItemButtonProps extends ComponentProps<"button"> {
     text: string
     class: string
 }
 
 export default function NewItemButton(props: NewItemButtonProps) {
+    const [local, ...[buttonProps]] = splitProps(props, ["text", "class"])
     return <button
         class="bg-stone-200 dark:bg-stone-900
             content-center items-center
@@ -16,8 +18,8 @@ export default function NewItemButton(props: NewItemButtonProps) {
             py-11 m-3
             h-20 min-w-[300px]
             rounded-xl
-            flex drop-shadow-md">
-        <BlockIcon icon={<BsPlusLg size={28}/>} class={props.class}/>
-        <span class="text-xl">{props.text}</span>
+            flex drop-shadow-md" {...buttonProps}>
+        <BlockIcon icon={<BsPlusLg size={28}/>} class={local.class}/>
+        <span class="text-xl">{local.text}</span>
     </button>
 }
