a21f9b801e4d856e2f42c68316bdbf04e137057a
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-01 14:58:31 +0300

    bind onClick to buttons directly

--- src/components/titlebar/TitleBar.tsx
+++ src/components/titlebar/TitleBar.tsx
@@ -2,28 +2,17 @@
 
 import {TbRectangle} from "solid-icons/tb";
 import {FaSolidMinus, FaSolidXmark} from "solid-icons/fa";
-import {onMount} from "solid-js";
 import {appWindow} from "@tauri-apps/api/window";
 
 export default function TitleBar() {
-    let minimizeButton: HTMLDivElement | undefined;
-    let maximizeButton: HTMLDivElement | undefined;
-    let closeButton: HTMLDivElement | undefined;
-
-    onMount(() => {
-        minimizeButton!.onclick = () => appWindow.minimize()
-        maximizeButton!.onclick = () => appWindow.toggleMaximize()
-        closeButton!.onclick = () => appWindow.close()
-    })
-
     return <div class="titlebar" data-tauri-drag-region>
-        <div class="titlebar-button" ref={minimizeButton}>
+        <div class="titlebar-button" onClick={() => appWindow.minimize()}>
             <FaSolidMinus/>
         </div>
-        <div class="titlebar-button" ref={maximizeButton}>
+        <div class="titlebar-button" onClick={() => appWindow.toggleMaximize()}>
             <TbRectangle/>
         </div>
-        <div class="titlebar-button hover:bg-red-500" ref={closeButton}>
+        <div class="titlebar-button hover:bg-red-500" onClick={() => appWindow.close()}>
             <FaSolidXmark/>
         </div>
     </div>
