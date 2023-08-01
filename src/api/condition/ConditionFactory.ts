e385f0aabd1aa060aa7c433bb26197d7d66be2e2
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-01 20:38:18 +0300

    register conditions in ConditionIDs.ts

--- src/api/condition/ConditionFactory.ts
+++ src/api/condition/ConditionFactory.ts
@@ -1,16 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any,no-unused-vars */
 
-import {BatteryCondition} from "~/conditions/BatteryCondition";
-import {TimeCondition} from "~/conditions/TimeCondition";
 import Condition from "~/api/condition/Condition";
-import {BATTERY_ID, TIME_ID} from "~/api/condition/ConditionIDs";
-
-register(BATTERY_ID, (data) => new BatteryCondition(data))
-register(TIME_ID, (data) => new TimeCondition(data))
 
 const idToCondition: Record<string, (data: any) => Condition> = {}
 
-function register(name: string, factory: (data: any) => Condition) {
+export function registerCondition(name: string, factory: (data: any) => Condition) {
     idToCondition[name] = factory
 }
 
--- src/api/condition/ConditionIDs.ts
+++ src/api/condition/ConditionIDs.ts
@@ -1,2 +1,9 @@
+import {BatteryCondition} from "~/conditions/BatteryCondition";
+import {TimeCondition} from "~/conditions/TimeCondition";
+import {registerCondition} from "~/api/condition/ConditionFactory";
+
 export const BATTERY_ID = "battery"
 export const TIME_ID = "time"
+
+registerCondition(BATTERY_ID, (data) => new BatteryCondition(data))
+registerCondition(TIME_ID, (data) => new TimeCondition(data))
