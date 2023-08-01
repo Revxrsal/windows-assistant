a379ebde56538f9369ebb87ac27a830d6164927c
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-01 20:35:53 +0300

    Rename a few sidebar items, make content more dynamic

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import SidebarItem from "~/components/sidebar/SidebarItem";
-import {BsDatabaseAdd, BsFolder, BsGear, BsPinMapFill} from "solid-icons/bs";
+import {BsDatabase, BsDatabaseAdd, BsFolder, BsGear, BsPinMapFill} from "solid-icons/bs";
 import SidebarTitle from "~/components/sidebar/SidebarTitle";
 import HorizontalDivider from "~/components/HorizontalDivider";
 import {useLocation} from "@solidjs/router";
 import SidebarSubtitle from "~/components/sidebar/SidebarSubtitle";
 import {FaSolidForwardFast} from "solid-icons/fa";
+import {For} from "solid-js";
+import Folders from "~/sample/Folders";
 
 export default function Sidebar() {
     const location = useLocation();
@@ -15,12 +17,12 @@ export default function Sidebar() {
                 <SidebarTitle title={"Windows Assistant"}/>
                 <HorizontalDivider/>
                 <SidebarItem
-                    label={"New"}
-                    icon={<BsDatabaseAdd size={24}/>}
+                    label={"Routines"}
+                    icon={<BsDatabase size={24}/>}
                     isActive={isPathOpen("/")}
                 />
                 <SidebarItem
-                    label={"My Shortcuts"}
+                    label="Shortcuts"
                     icon={<BsPinMapFill size={24}/>}
                     isActive={isPathOpen("/my-shortcuts")}
                 />
@@ -31,9 +33,9 @@ export default function Sidebar() {
                 />
 
                 <SidebarSubtitle title={"Folders"}/>
-                <SidebarItem label={"Travel"} icon={<BsFolder size={24}/>}/>
-                <SidebarItem label={"Photos"} icon={<BsFolder size={24}/>}/>
-                <SidebarItem label={"Fitness"} icon={<BsFolder size={24}/>}/>
+                <For each={Folders}>{folder =>
+                    <SidebarItem label={folder.name} icon={<BsFolder size={24}/>}/>}
+                </For>
             </div>
         </>
     )
