40e235e2dbd265f2f5ef660eb265e722b0ea90e3
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-01 20:42:59 +0300

    minor corrections in ConditionFactory.ts

--- src/api/condition/ConditionFactory.ts
+++ src/api/condition/ConditionFactory.ts
@@ -2,15 +2,30 @@
 
 import Condition from "~/api/condition/Condition";
 
+/**
+ * Maps the IDs of conditions to factory functions
+ */
 const idToCondition: Record<string, (data: any) => Condition> = {}
 
-export function registerCondition(name: string, factory: (data: any) => Condition) {
-    idToCondition[name] = factory
+/**
+ * Registers a condition with the given ID and factory function
+ *
+ * @param id ID of the condition
+ * @param factory The factory function of the condition
+ */
+export function registerCondition(id: string, factory: (data: any) => Condition) {
+    idToCondition[id] = factory
 }
 
-export function fromData<T extends Condition>(data: { id: number }): T {
+/**
+ * Constructs a condition from raw JSON data. This function must
+ * be called carefully, as it does no property validation.
+ *
+ * @param data JSON data
+ */
+export function conditionFromData<T extends Condition>(data: any): T {
     const factory = idToCondition[data.id]
     if (factory != undefined)
         return factory(data) as T
-    throw new Error(`Cannot find a condition with id ${data.id}`)
+    throw new Error(`Cannot find a condition with id '${data.id}'`)
 }
