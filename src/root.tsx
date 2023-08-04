be82ebc33ccecb3b38ba1608ec4117b1a7cf074f
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-04 14:41:01 +0300

    use the preferences API

--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,12 @@
 // @refresh reload
-import {createEffect, createSignal, onMount, Suspense} from "solid-js";
+import {onMount, Suspense} from "solid-js";
 import {Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title,} from "solid-start";
 import "./root.css";
 import Sidebar from "~/components/sidebar/Sidebar";
 import TitleBar from "~/components/titlebar/TitleBar";
 import {registerConditions} from "~/api/condition/ConditionRegistry";
 import {registerActions} from "~/api/action/ActionRegistry";
+import {preferences} from "~/prefs/preferences";
 
 /**
  * Registers blocks and conditions
@@ -15,28 +16,13 @@ function registerBlockTypes() {
     registerActions();
 }
 
-const getTheme = () => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        localStorage.theme = 'dark';
-        return 'dark'
-    } else {
-        localStorage.theme = 'light';
-        return 'light'
-    }
-}
-
-const [mode, setMode] = createSignal(getTheme())
-
 export default function Root() {
-
-    createEffect(() => {
-        localStorage.theme = mode()
-    })
-
     onMount(() => registerBlockTypes())
 
     return (
-        <Html lang="en" class={mode()}>
+        <Html lang="en" classList={{
+            dark: preferences.darkTheme
+        }}>
             <Head>
                 <Title>Windows Assistant</Title>
                 <Meta charset="utf-8"/>
