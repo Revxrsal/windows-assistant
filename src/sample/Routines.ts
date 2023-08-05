f5968bec4031a618abfd96527e1d2aa99f2e5a29
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-05 11:44:34 +0300

    load stored routines

--- src/sample/Routines.ts
+++ src/sample/Routines.ts
@@ -2,7 +2,16 @@ import Routine from "~/api/routine/Routine";
 import {createStore} from "solid-js/store";
 import {createEffect} from "solid-js";
 
-export const [routines, setRoutines] = createStore<Routine[]>([]);
+export const [routines, setRoutines] = createStore<Routine[]>(
+    loadStoredRoutines()
+);
+
+function loadStoredRoutines(): Routine[] {
+    const stored = localStorage?.getItem("routines")
+    if (stored == null)
+        return []
+    return JSON.parse(stored)
+}
 
 export function createEmptyRoutine(): Routine {
     return {actions: [], conditions: [], name: ""}
