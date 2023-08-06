07f9ca455c198a359c3b6ed2ca1805b6160546bb
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-06 22:29:30 +0300

    a bit of cleaning up

--- src/routes/routines/[id].tsx
+++ src/routes/routines/[id].tsx
@@ -2,18 +2,16 @@ import {useParams} from "@solidjs/router";
 import {setStorage, storage} from "~/sample/Routines";
 import RoutineForm from "~/components/routine/EditRoutineForm";
 import {createStore, produce, SetStoreFunction, Store} from "solid-js/store";
-import {createEffect, createRenderEffect, createSignal} from "solid-js";
+import {createRenderEffect, createSignal} from "solid-js";
 import Routine from "~/api/routine/Routine";
 
 export default function EditRoutine() {
     const params = useParams();
-    const routineIndex = storage.routines.findIndex(s => s.id == params.id)!;
+    const routineIndex = () => storage.routines.findIndex(s => s.id == params.id)!;
     const [routine, setRoutineSignal] = createSignal<Store<Routine>>()
-    const [setRoutine, setSetRoutine] =
-        createSignal<SetStoreFunction<Routine>>()
+    const [setRoutine, setSetRoutine] = createSignal<SetStoreFunction<Routine>>()
     createRenderEffect(() => {
-        const routineIndex = storage.routines.findIndex(s => s.id == params.id)!;
-        const store = createStore(storage.routines[routineIndex])
+        const store = createStore(storage.routines[routineIndex()])
         setRoutineSignal(store[0])
         setSetRoutine(() => store[1])
     })
@@ -22,7 +20,7 @@ export default function EditRoutine() {
         setRoutine={setRoutine()!}
         replace={true}
         onFinish={(routine) => {
-            setStorage("routines", produce((r) => r[routineIndex] = routine))
+            setStorage("routines", produce((r) => r[routineIndex()] = routine))
         }}
     />
 }
