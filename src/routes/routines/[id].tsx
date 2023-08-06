b6d1c2b68cf9a2cc489b5825c4a0cac0d05c93e2
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-06 22:27:06 +0300

    use a half-baked solution for navigating pages and maintaining separate data, until a better one appears

--- src/routes/routines/[id].tsx
+++ src/routes/routines/[id].tsx
@@ -1,20 +1,28 @@
-import {Params, useParams} from "@solidjs/router";
+import {useParams} from "@solidjs/router";
 import {setStorage, storage} from "~/sample/Routines";
 import RoutineForm from "~/components/routine/EditRoutineForm";
-import {createStore, produce} from "solid-js/store";
-import {createMemo} from "solid-js";
+import {createStore, produce, SetStoreFunction, Store} from "solid-js/store";
+import {createEffect, createRenderEffect, createSignal} from "solid-js";
+import Routine from "~/api/routine/Routine";
 
 export default function EditRoutine() {
     const params = useParams();
-    const id = createMemo(() => parseInt(params.id))
-    const routineIndex = () => storage.routines.findIndex(s => s.id == id())!;
-    const [routine, setRoutine] = createStore(storage.routines[routineIndex()])
+    const routineIndex = storage.routines.findIndex(s => s.id == params.id)!;
+    const [routine, setRoutineSignal] = createSignal<Store<Routine>>()
+    const [setRoutine, setSetRoutine] =
+        createSignal<SetStoreFunction<Routine>>()
+    createRenderEffect(() => {
+        const routineIndex = storage.routines.findIndex(s => s.id == params.id)!;
+        const store = createStore(storage.routines[routineIndex])
+        setRoutineSignal(store[0])
+        setSetRoutine(() => store[1])
+    })
     return <RoutineForm
-        routine={routine}
-        setRoutine={setRoutine}
+        routine={routine()!}
+        setRoutine={setRoutine()!}
         replace={true}
         onFinish={(routine) => {
-            setStorage("routines", produce((r) => r[routineIndex()] = routine))
+            setStorage("routines", produce((r) => r[routineIndex] = routine))
         }}
     />
 }
