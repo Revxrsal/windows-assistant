ea05ac2a903160fd896f2dd7345ec4abedc3c00a
Ori <38721999+OriSpirit@users.noreply.github.com>
2023-08-08 00:03:41 +0800

    uncomment because it works now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::error::Error;
+use std::time::Duration;
+use rodio::{Decoder, OutputStream, Sink};
+use rodio::source::{SineWave, Source};
+
 mod file;
 
 fn main() {
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![beep, file::open_file_dialog])
         .run(tauri::generate_context!())
@@ -12,5 +18,30 @@ fn main() {
 
 #[tauri::command]
 fn beep() {
-    // still need to figure out how to beep.
+    let (stream, handle) = match OutputStream::try_default() {
+        Ok(s) => (s.0, s.1),
+        Err(err) => {
+            eprintln!("Failed to fetch default output device: ");
+            eprintln!("{}", err.to_string());
+            return;
+        }
+    };
+
+    let sink = match Sink::try_new(&handle) {
+        Ok(sink) => sink,
+        Err(err) => {
+            eprintln!("Failed to create a new sink: ");
+            eprintln!("{}", err.to_string());
+            return;
+        }
+    };
+
+    let src = SineWave::new(440.0)
+        .take_duration(Duration::from_secs_f32(0.25))
+        .amplify(0.2);
+
+    sink.append(src);
+
+    // Blocks the thread until all queued sounds have been cleared.
+    // sink.sleep_until_end();
 }
--- src/routes/routines/[id].tsx
+++ src/routes/routines/[id].tsx
@@ -1,60 +1,26 @@
-import {useNavigate, useParams} from "@solidjs/router";
+import {useParams} from "@solidjs/router";
 import {setStorage, storage} from "~/sample/Routines";
+import {setFormRoutine} from "~/routes/routines/new";
 import RoutineForm from "~/components/routine/EditRoutineForm";
-import {createStore, produce, SetStoreFunction, Store} from "solid-js/store";
-import {createRenderEffect, createSignal} from "solid-js";
-import Routine from "~/api/routine/Routine";
-import Modal from "~/components/modal/Modal";
+import {createEffect, createRenderEffect, onMount} from "solid-js";
+import {produce} from "solid-js/store";
 
 export default function EditRoutine() {
     const params = useParams();
-    const navigate = useNavigate()
-    const routineIndex = () => storage.routines.findIndex(s => s.id == params.id)!;
-    const [routine, setRoutineSignal] = createSignal<Store<Routine>>()
-    const [setRoutine, setSetRoutine] = createSignal<SetStoreFunction<Routine>>()
-    const [confirmDialog, setConfirmDialog] = createSignal(false)
-    createRenderEffect(() => {
-        const [store, setStore] = createStore(storage.routines[routineIndex()])
-        setRoutineSignal(store)
-        setSetRoutine(() => setStore)
+    const routineIndex = storage.routines.findIndex(s => s.name == params.id)!;
+    const routine = storage.routines[routineIndex]
+    onMount(() => {
+        setFormRoutine(produce((v) => {
+            v.name = routine.name
+            v.actions = routine.actions
+            v.conditions = routine.conditions
+        }))
     })
-    return <>
-        <Modal
-            show={confirmDialog}
-            setShow={setConfirmDialog}
-            heading={
-                <h1 class="text-3xl font-bold">Are you sure?</h1>
-            }
-        >
-            <p>Are you sure you want to delete this routine? This action cannot be undone!</p>
-            <div class={"my-5"}/>
-            <div class={"flex flex-row justify-around"}>
-                <button
-                    class="bg-stone-300 dark:bg-stone-700 p-3 rounded text hover:scale-[1.03] transition"
-                    onClick={() => setConfirmDialog(false)}
-                >
-                    No, keep it
-                </button>
-                <button
-                    class="bg-red-500 p-3 rounded text-stone-200 hover:scale-[1.03] transition"
-                    onClick={() => {
-                        setConfirmDialog(false)
-                        setStorage("routines", v => v.filter(ir => ir.id !== routine()?.id))
-                        navigate("/routines")
-                    }}
-                >
-                    Yes, delete it
-                </button>
-            </div>
-        </Modal>
-        <RoutineForm
-            routine={{...routine()!}}
-            setRoutine={setRoutine()!}
-            replace={true}
-            onFinish={(routine) => {
-                setStorage("routines", produce((r) => r[routineIndex()] = routine))
-            }}
-            onDelete={() => setConfirmDialog(true)}
-        />
-    </>
+    createEffect(() => setFormRoutine(() => storage.routines[routineIndex]))
+    return <RoutineForm
+        replace={true}
+        onFinish={(routine) => {
+            setStorage("routines", produce((r) => r[routineIndex] = routine))
+        }}
+    />
 }
