9239cd0330db3cada5dd4e52e74f67c96e3b8cde
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-12 10:55:09 +0300

    create form for key combination

---
+++ src/api/condition/keyboard/KeyCombinationForm.tsx
@@ -0,0 +1,58 @@
+import {BlockFormProps} from "~/api/block/BlockMetadata";
+import {KeyCombinationCondition,} from "~/api/condition/keyboard/KeyCombinationCondition";
+import BlockForm from "~/api/condition/BlockForm";
+import {createStore} from "solid-js/store";
+import SwitchButton from "~/components/input/SwitchButton";
+import KeyInput from "~/components/input/KeyInput";
+import KeyCombinationData, {createEmptyCombination} from "~/api/condition/keyboard/KeyboardCombinationData";
+
+export default function KeyCombinationForm(props: BlockFormProps<KeyCombinationData>) {
+    const [data, setData] = createStore<KeyCombinationData>(
+        props.data ? {...props.data} : createEmptyCombination()
+    )
+
+    function submit() {
+        props.submit(new KeyCombinationCondition(data))
+    }
+
+    return <BlockForm
+        title={"Key combination"}
+        configProps={props}
+        submit={submit}
+        disabled={false}
+    >
+        <KeyInput data={data} setData={setData}/>
+        <div class={"grid grid-cols-2"}>
+            <SwitchButton
+                label="Alt"
+                checked={data.alt}
+                onClick={() => setData("alt", v => !v)}
+            />
+            <SwitchButton
+                label="Ctrl"
+                checked={data.ctrl}
+                onClick={() => setData("ctrl", v => !v)}
+            />
+            <SwitchButton
+                label="Tab"
+                checked={data.tab}
+                onClick={() => setData("tab", v => !v)}
+            />
+            <SwitchButton
+                label="Enter"
+                checked={data.enter}
+                onClick={() => setData("enter", v => !v)}
+            />
+            <SwitchButton
+                label="Shift"
+                checked={data.shift}
+                onClick={() => setData("shift", v => !v)}
+            />
+            <SwitchButton
+                label="Windows"
+                checked={data.windows}
+                onClick={() => setData("windows", v => !v)}
+            />
+        </div>
+    </BlockForm>
+}
---
+++ src/components/input/KeyInput.tsx
@@ -0,0 +1,79 @@
+import {createSignal, onCleanup} from "solid-js";
+import {reconcile, SetStoreFunction} from "solid-js/store";
+import KeyCombinationData, {
+    combinationToString,
+    createEmptyCombination
+} from "~/api/condition/keyboard/KeyboardCombinationData";
+
+export default function KeyInput(props: {
+    data: KeyCombinationData,
+    setData: SetStoreFunction<KeyCombinationData>
+}) {
+    const [listening, setListening] = createSignal(false);
+    let lastInput = Date.now()
+
+    function listener(event: KeyboardEvent) {
+        if (listening()) {
+            if (event.code === "Escape") {
+                setListening(false);
+                return;
+            }
+            const time = Date.now()
+            if (time - lastInput >= 1000) {
+                props.setData(reconcile(createEmptyCombination()))
+            }
+            lastInput = Date.now()
+            const code = event.code
+
+            if (event.ctrlKey)
+                props.setData("ctrl", true)
+
+            if (event.shiftKey)
+                props.setData("shift", true)
+
+            if (event.altKey)
+                props.setData("alt", true)
+
+            if (code === "Tab")
+                props.setData("tab", true);
+
+            if (code === "Enter")
+                props.setData("enter", true)
+
+            if (event.metaKey)
+                props.setData("windows", true)
+
+            if (code.startsWith("Alt") || code.startsWith("Control")
+                || code.startsWith("Enter") || code.startsWith("Tab")
+                || code.startsWith("Meta") || code.startsWith("Shift"))
+                return;
+            if (code.startsWith("Key")) {
+                props.setData("key", code.substring(3))
+            } else if (code.startsWith("Digit")) {
+                props.setData("key", code.substring(5))
+            } else {
+                props.setData("key", code)
+            }
+            // noinspection JSDeprecatedSymbols
+            props.setData("keyCode", event.keyCode)
+        }
+    }
+
+    document.addEventListener("keydown", listener)
+    onCleanup(() => document.removeEventListener("keydown", listener))
+    return (
+        <div class={"flex flex-col justify-around content-center items-center"}>
+            <p class={"mb-5 text text-3xl"}>
+                {combinationToString(props.data)}
+            </p>
+            <button class={"bg-rose-900 text-stone-200 p-3 rounded transition-all w-32 focus:outline-none"} classList={{
+                "bg-rose-800": listening(),
+                "ring": listening(),
+                "ring-red-500": listening()
+            }}
+                    onClick={() => setListening(v => !v)}>
+                {listening() ? "Recording..." : "Record"}
+            </button>
+        </div>
+    );
+}
