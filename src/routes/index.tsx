cc6808cccdd755146c1822ecc1bbd14833845b1c
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-12 22:07:42 +0300

    remove the home modal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,13 @@
-import {createSignal} from "solid-js";
-import Modal from "~/components/modal/Modal";
 import Title from "~/components/text/Title";
 
 export default function Home() {
-    const [show, setShow] = createSignal(false);
     return (
         <main>
             <Title size={6}>Windows Assistant</Title>
             <Title size={4}>Getting started</Title>
-            <button
-                class="mx-12 bg-blue-600 w-32 text-stone-200 h-10 rounded hover:scale-105 transition"
-                onClick={() => setShow(true)}
-            >
+            <button class="mx-12 bg-blue-600 w-32 text-stone-200 h-10 rounded hover:scale-105 transition">
                 Open modal
             </button>
-            <Modal
-                show={show}
-                setShow={setShow}
-                heading={<h1 class="text-3xl font-bold">Hello</h1>}
-            >
-                <h1>Hello</h1>
-            </Modal>
         </main>
     )
 }
