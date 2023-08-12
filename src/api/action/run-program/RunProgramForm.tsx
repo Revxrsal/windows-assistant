dc7af7cfeee1c37413038bc8fd83c8a886d254fd
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-12 17:29:56 +0300

    refactor the file drop handler into a separate component

--- src/api/action/run-program/RunProgramForm.tsx
+++ src/api/action/run-program/RunProgramForm.tsx
@@ -1,15 +1,12 @@
 import {BlockFormProps} from "~/api/block/BlockMetadata";
 import RunProgramAction, {RunProgramData} from "~/api/action/run-program/RunProgramAction";
 import BlockForm from "~/api/condition/BlockForm";
-import {createMemo, createSignal, onCleanup} from "solid-js";
-import {listen, TauriEvent, UnlistenFn} from "@tauri-apps/api/event";
-import {invoke} from "@tauri-apps/api";
-import {getBaseFileName} from "~/api/utils/utils";
+import {createSignal} from "solid-js";
+import FileInput from "~/components/input/FileInput";
 
 export default function RunProgramForm(props: BlockFormProps<RunProgramData>) {
     const [path, setPath] = createSignal(props.data?.path || "")
     const [args, setArgs] = createSignal(props.data?.args || "")
-    const fileName = createMemo(() => getBaseFileName(path()))
 
     function submit() {
         props.submit(new RunProgramAction({
@@ -18,36 +15,17 @@ export default function RunProgramForm(props: BlockFormProps<RunProgramData>) {
         }))
     }
 
-    const [removeListener, setRemoveListener] = createSignal<UnlistenFn>()
 
-    listen(TauriEvent.WINDOW_FILE_DROP, event => {
-        const path = (event.payload as string[])[0]
-        setPath(path)
-    }).then(v => setRemoveListener(() => v))
-    onCleanup(() => removeListener()?.())
     return <BlockForm
         configProps={props}
         submit={submit}
         title="Run a program"
         disabled={path().length == 0}
     >
-        <div class={`
-            bg-stone-200 dark:bg-stone-900 hover:scale-[1.02] transition
-            content-center items-center text-center justify-center cursor-pointer select-none
-            opacity-60
-            outline-dotted outline-stone-400
-            text font-semibold
-            h-20 min-w-[300px]
-            rounded-xl
-            flex drop-shadow-md`}
-             onClick={async () => {
-                 const file: string = await invoke("open_file_dialog")
-                 setPath(file)
-             }}
-        >
-            <span
-                class={"text-xl text-center px-4"}>{fileName() || `Drop your program here, or click to choose one`}</span>
-        </div>
+        <FileInput
+            path={path}
+            setPath={setPath}
+        />
         <>
             <label for="args">
                 Run arguments (optional)
---
+++ src/components/input/FileInput.tsx
@@ -0,0 +1,39 @@
+import {invoke} from "@tauri-apps/api";
+import {Accessor, createMemo, onCleanup, Setter} from "solid-js";
+import {getBaseFileName} from "~/api/utils/utils";
+import {listen, TauriEvent, UnlistenFn} from "@tauri-apps/api/event";
+
+export interface FileInputProps {
+    path: Accessor<string>,
+    setPath: Setter<string>
+}
+
+export default function FileInput(props: FileInputProps) {
+    const fileName = createMemo(() => getBaseFileName(props.path()))
+
+    let removeListener: UnlistenFn;
+    listen(TauriEvent.WINDOW_FILE_DROP, event => {
+        const path = (event.payload as string[])[0]
+        props.setPath(path)
+    }).then(v => removeListener = v)
+
+    onCleanup(() => removeListener())
+
+    return <div class={`
+            bg-stone-200 dark:bg-stone-900 hover:scale-[1.02] transition
+            content-center items-center text-center justify-center cursor-pointer select-none
+            opacity-60
+            outline-dotted outline-stone-400
+            text font-semibold
+            h-20 min-w-[300px]
+            rounded-xl
+            flex drop-shadow-md`}
+                onClick={async () => {
+                    const file: string = await invoke("open_file_dialog")
+                    props.setPath(file)
+                }}
+    >
+            <span
+                class={"text-xl text-center px-4"}>{fileName() || `Drop your program here, or click to choose one`}</span>
+    </div>;
+}
