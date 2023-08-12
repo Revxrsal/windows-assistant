5bf7c4a5d2096f4bf2e333bca88cd0fc70a48d45
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-12 10:55:18 +0300

    register key combination condition

--- src/api/condition/ConditionRegistry.ts
+++ src/api/condition/ConditionRegistry.ts
@@ -1,10 +1,12 @@
 import BlockFactory from "~/api/block/BlockFactory";
 import {BatteryLevelCondition} from "~/api/condition/battery/BatteryLevelCondition";
 import {IsChargingCondition} from "~/api/condition/battery/IsChargingCondition";
+import {KeyCombinationCondition} from "~/api/condition/keyboard/KeyCombinationCondition";
 
 export const conditions = new BlockFactory("condition")
 
 export function registerConditions() {
     BatteryLevelCondition.register(conditions)
     IsChargingCondition.register(conditions)
+    KeyCombinationCondition.register(conditions)
 }
