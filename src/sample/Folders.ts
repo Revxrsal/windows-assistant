859720ba640384bf79f1fc8a31a256c61312eb97
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-13 09:48:51 +0300

    remove files

--- src/sample/Folders.ts
+++
@@ -1,9 +0,0 @@
-import Folder, {createFolder} from "~/api/folders/Folder";
-
-const Folders: Folder[] = [
-    createFolder("Photos"),
-    createFolder("Travel"),
-    createFolder("Fitness"),
-]
-
-export default Folders;
