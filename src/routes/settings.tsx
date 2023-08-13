e7ee20b2ac427e1fb4bee5f3a1ffc2ca6eff99de
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-13 20:13:56 +0300

    fix settings not working, refactor a bit

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,28 +1,39 @@
 import {preferences, setPreferences} from "~/storage/preferences";
 import SwitchButton from "~/components/input/SwitchButton";
 import Title from "~/components/text/Title";
-import {INVERT} from "~/util/Setter";
+import {ComponentProps, splitProps} from "solid-js";
 
-
-export default async function Settings() {
-
+export default function Settings() {
     return (
         <main>
             <Title size={6}>Settings</Title>
-            <Title size={4}>Startup</Title>
-            <SwitchButton
-                checked={preferences.autoStart}
-                onClick={() => setPreferences("autoStart", INVERT)}
-                label={"Autostart"}
-                class={"m-12 mt-8"}/>
-
             <Title size={4}>Appearance</Title>
-            <SwitchButton
-                checked={preferences.darkTheme}
-                onClick={() => setPreferences("darkTheme", INVERT)}
-                label="Dark Mode"
-                class={"m-12 mt-8"}
-            />
+            <SettingsArea>
+                <SwitchButton
+                    checked={preferences.darkTheme}
+                    onClick={() => setPreferences("darkTheme", v => !v)}
+                    label="Dark Mode"
+                    class={"m-12 mt-8"}
+                />
+            </SettingsArea>
+            <Title size={4}>Startup</Title>
+            <SettingsArea>
+                <SwitchButton
+                    checked={preferences.autoStart}
+                    onClick={() => setPreferences("autoStart", v => !v)}
+                    label={"Run on start"}
+                    class={"m-12 mt-8"}/>
+            </SettingsArea>
         </main>
     )
+}
+
+interface SettingsAreaProps extends ComponentProps<"div"> {
+    class?: string
+}
+
+
+function SettingsArea(props: SettingsAreaProps) {
+    const [local, divProps] = splitProps(props, ["class"]);
+    return <div class={`px-6 ${local.class}`} {...divProps}/>
 }
