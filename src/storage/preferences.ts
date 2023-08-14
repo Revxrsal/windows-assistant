b01588097e767530792d3f7ae6c83117d2572283
Ori <38721999+OriSpirit@users.noreply.github.com>
2023-08-14 10:24:19 +0800

    update preferences.ts on createDefaultPreferences

--- src/storage/preferences.ts
+++ src/storage/preferences.ts
@@ -15,7 +15,7 @@ export const [preferences, setPreferences] = createStore<Preferences>(
 function createDefaultPreferences(): Preferences {
     // Default enable
     isEnabled().then(cb => {
-        if(cb) return;
+        if(cb || !preferences.autoStart) return;
         enable().then();
     })
     return {
