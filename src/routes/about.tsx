56be4c83457e38fa77d0fc9a072d892a42c05ff7
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-20 16:21:52 +0300

    add a simple about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,3 +1,21 @@
-export default function About() {
-    return <main></main>;
+import Column from "~/components/layout/Column";
+import Header from "~/components/text/Header";
+import {getTauriVersion, getVersion} from "@tauri-apps/api/app";
+import {createResource} from "solid-js";
+import Text from "~/components/text/Text";
+
+export default () => {
+    const [version] = createResource(getVersion)
+    const [tauriVersion] = createResource(getTauriVersion)
+    return (
+        <main>
+            <Column>
+                <Header size={3}>Windows Assistant</Header>
+                <Text class={"mx-12"}>Version {version()}</Text>
+
+                <Header size={3}>Tauri</Header>
+                <Text class={"mx-12"}>Version {tauriVersion()}</Text>
+            </Column>
+        </main>
+    );
 }
